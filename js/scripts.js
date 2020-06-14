194d1316883a1d574762330691cfa6e0f6759fa5
Kelei <renkelei.kelly@gmail.com>
2020-06-14 12:06:18 +0300

    mile stone 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
-//  Features
-// Instead of hardcoding Y (the result of the Fibonacci of X), calculate it with a for loop
-// The calculation should be wrapped in a function, that gets X as an argument, and returns Y
-// After the function, you should call the function, and assign the returned value in you HTML to present to the user
+// Milestone 3
 
-let x = 5;
+// Features
+// Create an input (with number type) element and a button next to that calculates fibonacci.
+// Follow this figma design (it is based on bootstrap, so use bootstrap): ITC Fibonacci Project Design  (For this milestone, only the first row of screens is relevant)
+// Add a click event listener to the button, that executes a function that takes the number value in the created input, calculates it’s Fibonacci value and presents it to the user
+// Recommended googling: ‘HTML Input element’ and ‘get value in javascript of an input element’
+// Milestone 3.1 - Geekout
+
+// Features
+// Implement the Fibonacci function with recursion
+// We know you can google it and copy the code, try doing it by yourself (go over the lectures/youtube if needed)
+
+let x;
 let y;
-
 function fibonacciCalc(x) {
   if (x === 0) {
     return 0;
   } else if (x === 1) {
     return 1;
-  } else {
+  } else if (x > 1) {
     y = fibonacciCalc(x - 1) + fibonacciCalc(x - 2);
     return y;
+  } else {
+    return "out of scope";
   }
 }
 
-let xOutPut = document.getElementById("numX");
-let yOutPut = document.getElementById("numY");
-xOutPut.innerText = x;
-yOutPut.innerText = fibonacciCalc(x);
+//get button callback getButton function once firing the click event
+document.getElementById("calculate").addEventListener("click", getButton);
+
+//get x input value
+function getButton() {
+  let numberX = parseInt(document.getElementById("numX").value);
+  let yOutPut = document.getElementById("numY");
+  yOutPut.innerText = fibonacciCalc(numberX);
+}
