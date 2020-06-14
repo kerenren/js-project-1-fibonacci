f5e4d7fb640e15faecced88f80e7e63083a2cf41
Kelei <renkelei.kelly@gmail.com>
2020-06-14 22:44:18 +0300

    please ignore: mile stone 5- with self notes version

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,46 @@
+let loader = document.getElementById("loader");
+let errorBox = document.getElementById("error");
+
+function present(selector) {
+  selector.classList.remove("d-none");
+}
+
+function unpresent(selector) {
+  selector.classList.add("d-none");
+}
+
 function getButton() {
-  let number = parseInt(document.getElementById("numX").value);
-  let itcServer = "http://localhost:5050/fibonacci/" + number.toString();
+  let number = document.getElementById("numX");
   let yOutPut = document.getElementById("numY");
-  fetch(itcServer)
-    .then((response) => response.json())
-    .then((data) => (yOutPut.innerText = data.result));
+  let itcServer = "http://localhost:5050/fibonacci/" + number.value;
+
+  if (parseInt(number.value) >= 50) {
+    present(errorBox);
+    errorBox.innerText = "400 (Bad Request)";
+  } else {
+    present(loader);
+    // selfnote for debug: console.log("loader is presented");
+    fetch(itcServer)
+      .then(function (response) {
+        // console.log(response);
+        if (response.ok === false || response.status !== 200) {
+          errorBox.innerText = response.statusText;
+          present(errorBox);
+          unpresent(loader);
+        } else {
+          unpresent(errorBox);
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        // console.log(data);
+        yOutPut.innerText = data.result;
+        unpresent(loader);
+        // selfnote for debug: console.log("loader is unpresented");
+      });
+  }
 }
 
 //get button callback getButton function once firing the click event
-document.getElementById("calculate").addEventListener("click", getButton);
+let calBtn = document.getElementById("calculate");
+calBtn.addEventListener("click", getButton);
