255e91576cae9e8a17f1901f44dbac08ac26c9bc
Kelei <renkelei.kelly@gmail.com>
2020-06-16 18:00:50 +0300

    Mile Stone 6 Batch 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,75 @@
 (() => {
   let loader = document.getElementById("loader");
+  let loader2 = document.getElementById("loader2");
   let errorBox = document.getElementById("error");
   let calBtn = document.getElementById("calculate");
   let errorMsg = document.getElementById("error-msg");
+  let result = document.getElementById("result");
+  let loadURL = "http://localhost:5050/getFibonacciResults";
 
   function present(selector) {
     selector.classList.remove("d-none");
   }
+
   function unpresent(selector) {
     selector.classList.add("d-none");
   }
 
   function calcButton() {
-    let number = document.getElementById("inputNum");
+    let numberFib = document.getElementById("inputNum");
     let yOutPut = document.getElementById("resultNum");
-    let itcServer = "http://localhost:5050/fibonacci/" + number.value;
+    let itcServer = "http://localhost:5050/fibonacci/" + numberFib.value;
+    let newResult = {
+      text: numberFib.value,
+      dateCreated: Date.now(),
+    };
+
+    function loadResults() {
+      fetch(loadURL)
+        .then((response) => response.json())
+        .then((data) => {
+          let resultObj = data.results;
+          console.log(resultObj);
+          for (let i = 0; i < resultObj.length; i++) {
+            let arrayFib = resultObj[i];
+            let calDate = new Date();
+            let li = document.createElement("li");
+            li.classList.add(
+              "border-bottom",
+              "mb-3",
+              "border-dark",
+              "d-inline-block",
+              "pb-3"
+            );
+            li.innerHTML = `The Fibonacci of <b>${arrayFib.number}</b> is <b>${arrayFib.result}</b>. Calculated at ${calDate}`;
+            result.prepend(li);
+          }
+          return resultObj;
+        });
+    }
+
+    loadResults();
+
+    fetch(loadURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/JSON",
+      },
+      body: JSON.stringify(newResult),
+    }).then((response) => {
+      if (response.ok) {
+        loadResults();
+      }
+    });
 
     function highlightError() {
-      number.classList.add("error-input");
+      numberFib.classList.add("error-input");
       present(errorBox);
       yOutPut.innerText = "";
     }
 
     function removeError() {
-      number.classList.remove("error-input");
+      numberFib.classList.remove("error-input");
       unpresent(errorMsg);
       unpresent(errorBox);
     }
@@ -33,22 +79,34 @@
       present(errorMsg);
       unpresent(errorBox);
       unpresent(loader);
+      unpresent(loader2);
     }
 
+    function presentLoader() {
+      present(loader);
+      present(loader2);
+    }
+
+    function unpresentLoader() {
+      unpresent(loader);
+      unpresent(loader2);
+    }
     removeError();
 
-    if (parseInt(number.value) > 50) {
+    if (parseInt(numberFib.value) > 50) {
       highlightError();
       errorBox.innerText = "Can't be larger than 50";
+    } else if (numberFib.value === "") {
+      yOutPut.innerText = "";
     } else {
-      present(loader);
+      presentLoader();
       fetch(itcServer)
         .then(function (response) {
           if (!response.ok) {
             handleError();
             return response.text();
           } else {
-            number.classList.remove("error-input");
+            numberFib.classList.remove("error-input");
             return response.json();
           }
         })
@@ -56,9 +114,10 @@
           if (typeof data === "string") {
             errorMsg.innerText = data;
           } else {
+            // console.log(data);
             yOutPut.innerText = data.result;
           }
-          unpresent(loader);
+          unpresentLoader();
         });
     }
   }
