d69ecf8cbe5844b9131a5e5de6a9197bd382a49c
Kelei <renkelei.kelly@gmail.com>
2020-06-17 18:33:01 +0300

    Mile stone 7 batch 1 added new check box

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@
   let errorMsg = document.getElementById("error-msg");
   let result = document.getElementById("result");
   let loadURL = "http://localhost:5050/getFibonacciResults";
+  let numberFib = document.getElementById("inputNum");
+  let yOutPut = document.getElementById("resultNum");
 
   function present(selector) {
     selector.classList.remove("d-none");
@@ -15,96 +17,73 @@
     selector.classList.add("d-none");
   }
 
-  function calcButton() {
-    let numberFib = document.getElementById("inputNum");
-    let yOutPut = document.getElementById("resultNum");
-    let itcServer = "http://localhost:5050/fibonacci/" + numberFib.value;
+  function pushList(inputNumber, calResult, calTime) {
+    let li = document.createElement("li");
+    li.classList.add(
+      "border-bottom",
+      "mb-3",
+      "border-dark",
+      "d-inline-block",
+      "pb-3"
+    );
+    li.innerHTML = `The Fibonacci of <b>${inputNumber}</b> is <b>${calResult}</b>. Calculated at ${calTime}`;
+    result.prepend(li);
+  }
+
+  function loadResults() {
+    fetch(loadURL)
+      .then((response) => response.json())
+      .then((data) => {
+        let resultObj = data.results;
+        console.log(resultObj);
+        for (let i = 0; i < resultObj.length; i++) {
+          let arrayFib = resultObj[i];
+          pushList(
+            arrayFib.number,
+            arrayFib.result,
+            new Date(arrayFib.createdDate)
+          );
+        }
+        return resultObj;
+      });
+  }
+
+  function highlightError() {
+    numberFib.classList.add("error-input");
+    present(errorBox);
+    yOutPut.innerText = "";
+  }
+
+  function removeError() {
+    numberFib.classList.remove("error-input");
+    unpresent(errorMsg);
+    unpresent(errorBox);
+  }
+
+  function handleError() {
+    highlightError();
+    present(errorMsg);
+    unpresent(errorBox);
+    unpresent(loader);
+    unpresent(loader2);
+  }
+
+  function presentLoader() {
+    present(loader);
+    present(loader2);
+  }
+
+  function unpresentLoader() {
+    unpresent(loader);
+    unpresent(loader2);
+  }
+
+  function calServer() {
+    let itcServer = "http://localhost:5050/fibonacci/" + numberFib.value; //? why move itcServer decleration outside calcButton function will remove all the errors alerts(e.g. number 42 error, number > 50 alert)?
     let newResult = {
       text: numberFib.value,
       dateCreated: new Date(),
     };
-
-    function pushList(inputNumber, calResult, calTime) {
-      let li = document.createElement("li");
-      li.classList.add(
-        "border-bottom",
-        "mb-3",
-        "border-dark",
-        "d-inline-block",
-        "pb-3"
-      );
-      li.innerHTML = `The Fibonacci of <b>${inputNumber}</b> is <b>${calResult}</b>. Calculated at ${calTime}`;
-      result.prepend(li);
-    }
-    function loadResults() {
-      fetch(loadURL)
-        .then((response) => response.json())
-        .then((data) => {
-          let resultObj = data.results;
-          console.log(resultObj);
-          for (let i = 0; i < resultObj.length; i++) {
-            let arrayFib = resultObj[i];
-            pushList(
-              arrayFib.number,
-              arrayFib.result,
-              new Date(arrayFib.createdDate)
-            );
-          }
-          return resultObj;
-        });
-    }
-
-    loadResults();
-
-    // function pushResult() {
-    //   fetch(loadURL, {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(newResult),
-    //   }).then((response) => {
-    //     if (response.ok) {
-    //       console.log(response);
-    //     } else {
-    //       console.log(response, "post is not wroking");
-    //     }
-    //   });
-    // }
-
-    // pushResult();
-
-    function highlightError() {
-      numberFib.classList.add("error-input");
-      present(errorBox);
-      yOutPut.innerText = "";
-    }
-
-    function removeError() {
-      numberFib.classList.remove("error-input");
-      unpresent(errorMsg);
-      unpresent(errorBox);
-    }
-
-    function handleError() {
-      highlightError();
-      present(errorMsg);
-      unpresent(errorBox);
-      unpresent(loader);
-      unpresent(loader2);
-    }
-
-    function presentLoader() {
-      present(loader);
-      present(loader2);
-    }
-
-    function unpresentLoader() {
-      unpresent(loader);
-      unpresent(loader2);
-    }
-    removeError();
-
     if (parseInt(numberFib.value) > 50) {
       highlightError();
       errorBox.innerText = "Can't be larger than 50";
@@ -133,6 +112,12 @@
           unpresentLoader();
         });
     }
+  }
+
+  function calcButton() {
+    removeError(); //? why removedError() function can not be placed after the fetch bellow
+    loadResults();
+    calServer();
   }
 
   calBtn.addEventListener("click", calcButton);
